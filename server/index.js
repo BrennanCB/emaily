7d0e4aff27120b3d25b945afe67d09b4de0deb4f
Brennan <brennanbuitendag@gmail.com>
2021-06-19 17:20:58 +0200

    - fix: Update default port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 console.log('I FOUND PORT: ', process.env.port);
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 console.log('I FOUND PORT: ', PORT);
 
 app.listen(PORT);
